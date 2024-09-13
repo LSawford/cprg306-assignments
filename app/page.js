10b480950497e4de644480c67fe81a4d76fb3391
LSawford <lucas.sawford@icloud.com>
2024-09-13 10:00:36 -0600

    Styling
    Made small styling changes to each page

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="./week-2">Week 2</Link>
+      <h1 className="text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="./week-2">Week 2 Assignment</Link>
     </main>
   );
 }
